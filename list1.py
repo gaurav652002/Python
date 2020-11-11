d3dd1a3cf05f00355550187fbc2b23131b33868f
gaurav652002 <warrior.prince652002@gmail.com>
2020-11-11 22:19:00 +0530

    list basics

---
+++ list1.py
@@ -0,0 +1,6 @@
+guns=["m4a4","m416","magnum","mk14","mk13","berrett","colt.45","revolver"]
+print("my choice of guns if i ever wanted to join an airsoft games")
+print(guns[1])
+print(guns[2])
+print(guns[6])
+print(guns[7])
