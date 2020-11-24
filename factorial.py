80ad3c4c3a47b142588ac6d9cb5c2b77d593ca26
gaurav652002 <warrior.prince652002@gmail.com>
2020-11-24 10:23:35 +0530

    pattern and factorial

---
+++ factorial.py
@@ -0,0 +1,5 @@
+#so this is file that  i have made to  revise all the concept i have learned till now 
+a=int(input("enter the number here"))
+for i in range (1,5,1):
+    a *= i
+print(a)
---
+++ pattern.py
@@ -0,0 +1,4 @@
+final = int(input("please enter the final term for the pattern"))
+for i  in range(1,final+1,1):
+    print("*"*i)
+
