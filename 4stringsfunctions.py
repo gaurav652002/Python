8afe894cb37b7928ed05e19d603910faf548633b
gaurav652002 <warrior.prince652002@gmail.com>
2020-11-29 00:40:40 +0530

    some loop and class codes

---
+++ 4stringsfunctions.py
@@ -0,0 +1,10 @@
+a="hello"
+b= len(a)
+print(a[0])
+print(b)
+c=a[::-1]
+print(c)
+d=a[1]+a[2]
+print(d)
+e=d.pop(3)
+print(e)
---
+++ classandobject1.py
@@ -0,0 +1,30 @@
+#this is a simple program to show the working of class and objects
+class ArtificialIntelligence():
+    def  self_introduction(self):
+        print("Hello! there!, My names is "+self.name+"Im an npc of soulShiftHorizon we may cross paths from now on so lets know each other! \n")
+        print("I am a "+self.type+"of AI")
+        print("my features are "+self.features+"i am an exclusive Ai just like a normal human being, there is no other Ai which is exacly same as my self \n")
+
+#an npc of the class artificial Intelegence 
+
+npc_1= ArtificialIntelligence()
+npc_1. name="Aoi \n"
+npc_1.type="common domestic Ai \n"
+npc_1.features="can detect human emotions and answer your questions effectively, understands and can react in the same way as a human would. Capable of falling in love and expressing eemotions freely \n"
+
+# another npc of the class artificial inteligence 
+npc_2=ArtificialIntelligence()
+npc_2.name="May \n"
+npc_2.type="young girl adventurer Ai \n"
+npc_2.features="capable of putting up fight against bandits and enemies, lacks magical support, but can support you in various fights. Remeber this is also a unique Ai so you will on find only one May in the entire SoulShiftHorizon cause this is the game of infinite possiblities. We have tried to make this game just close to the real world \n"
+#npc 3
+npc_3=ArtificialIntelligence()
+npc_3.name="Silvie \n"
+npc_3.type="your partner in this journey, you build up you trust and relations as you like with her \n"
+npc_3.features="she is you personal guide and companion while you are in the game. This is also a excusive Ai which is  just for you. the face design is done according to you character design but sometimes it may be completly random \n"
+# here i am using the object and the fucntion together
+npc_1.self_introduction()
+#this is for npc
+npc_2.self_introduction()
+#npc3
+npc_3.self_introduction()
