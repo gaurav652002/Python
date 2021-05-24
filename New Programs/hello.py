4d72a3c77e76ed724c15acfc2c8457d85edacc99
gaurav652002 <warrior.prince652002@gmail.com>
2021-05-24 12:42:57 +0530

    changed

--- New Programs/hello.py
+++ New Programs/hello.py
@@ -1,3 +1,3 @@
 print("hello world. This is what you expected wasn't it but sorry this is the second time im creating a new folder for the repository")
 print("because my previous work was all over the place and i want to create a new proper folder for future use")
-print("dont worry this will have more sub folders :)"
+print("dont worry this will have more sub folders")
